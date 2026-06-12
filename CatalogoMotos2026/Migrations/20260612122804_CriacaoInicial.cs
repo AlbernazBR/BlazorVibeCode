dfa4e50a00ec6372f179dc6225ce1727f1a99037
SYLVIO ALBERNAZ MACHADO NETO <saneto2@latam.stefanini.com>
2026-06-12 09:29:36 -0300

    .

---
+++ CatalogoMotos2026/Migrations/20260612122804_CriacaoInicial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogoMotos2026.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Motocicletas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Modelo = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Familia = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Cilindrada = table.Column<int>(type: "INTEGER", nullable: false),
+                    Ano = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 2026)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Motocicletas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Motocicletas");
+        }
+    }
+}
